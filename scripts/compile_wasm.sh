a20198e61de0c9fd5a9ca509140b9b8585deeea6
ujnss <unlinunlin@163.com>
2023-03-06 08:35:24 +0000

    update

--- scripts/compile_wasm.sh
+++ scripts/compile_wasm.sh
@@ -32,6 +32,20 @@ for repo in ${emp_repos[@]}; do
     export EMCC_CFLAGS="-O3 -s WASM=1 -msimd128 -s EXPORTED_FUNCTIONS=_main,_run,_run1"
     export EMCC_CFLAGS="-O3 -s WASM=1 -msimd128 -s LEGALIZE_JS_FFI=0 -sERROR_ON_UNDEFINED_SYMBOLS=0"
     export EMCC_CFLAGS="-O3 -s WASM=1 -msimd128"
+
+    EMCC_CFLAGS="-O3 -s WASM=1"
+
+    # sse* simd
+    SIMD_CFLAGS=" -msimd128"
+    EMCC_CFLAGS+=${SIMD_CFLAGS}
+
+    # WebSocket/POSIX Socket
+    SOCKET_FLAGS=" -lwebsocket.js -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD"
+    # EMCC_CFLAGS+=${SOCKET_FLAGS}
+
+    export EMCC_CFLAGS=${EMCC_CFLAGS}
+    echo "EMCC_CFLAGS: ${EMCC_CFLAGS}"
+
     # -DCMAKE_CROSSCOMPILING_EMULATOR=$NODE_HOME/bin/node
     emcmake cmake ${curdir}/${repo} \
       -DCMAKE_INSTALL_PREFIX=${builddir} \
