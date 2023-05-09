b982f61a2a788d01657656f352971485b7e8211f
ujnss <unlinunlin@163.com>
2023-05-09 15:10:11 +0800

    opt clmul

--- emp-tool/emp-tool/utils/__clmul.h
+++ emp-tool/emp-tool/utils/__clmul.h
@@ -34,8 +34,8 @@ dst[127] := 0
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
-// typedef unsigned __int128 uint128_t;
 
+#ifdef use_qclmulq_v1
 // static int ___mm_clmulepi64_si128_helper(uint8_t dst[16], uint8_t TEMP1[8], uint8_t TEMP2[8])
 static int ___mm_clmulepi64_si128_helper(uint8_t *dst, uint8_t *TEMP1, uint8_t *TEMP2)
 {
@@ -195,5 +195,62 @@ static void ___mm_clmulepi64_si128(uint8_t *c, uint8_t *a, uint8_t *b, int imm8)
     ___mm_clmulepi64_si128_helper(c, temp1, temp2);
   }
 }
+#else
+
+static uint64_t __clmul_u64(uint64_t x, uint64_t y)
+{
+  uint64_t x0, x1, x2, x3;
+  uint64_t y0, y1, y2, y3;
+  uint64_t z0, z1, z2, z3;
+
+  x0 = x & UINT64_C(0x1111111111111111);
+  x1 = x & UINT64_C(0x2222222222222222);
+  x2 = x & UINT64_C(0x4444444444444444);
+  x3 = x & UINT64_C(0x8888888888888888);
+  y0 = y & UINT64_C(0x1111111111111111);
+  y1 = y & UINT64_C(0x2222222222222222);
+  y2 = y & UINT64_C(0x4444444444444444);
+  y3 = y & UINT64_C(0x8888888888888888);
+
+  z0 = (x0 * y0) ^ (x1 * y3) ^ (x2 * y2) ^ (x3 * y1);
+  z1 = (x0 * y1) ^ (x1 * y0) ^ (x2 * y3) ^ (x3 * y2);
+  z2 = (x0 * y2) ^ (x1 * y1) ^ (x2 * y0) ^ (x3 * y3);
+  z3 = (x0 * y3) ^ (x1 * y2) ^ (x2 * y1) ^ (x3 * y0);
+
+  z0 &= UINT64_C(0x1111111111111111);
+  z1 &= UINT64_C(0x2222222222222222);
+  z2 &= UINT64_C(0x4444444444444444);
+  z3 &= UINT64_C(0x8888888888888888);
+
+  return z0 | z1 | z2 | z3;
+}
+
+static uint64_t __bitreverse_u64(uint64_t v)
+{
+  // clang-format off
+    v = ((v >>  1) & UINT64_C(0x5555555555555555)) | ((v & UINT64_C(0x5555555555555555)) <<  1);
+    v = ((v >>  2) & UINT64_C(0x3333333333333333)) | ((v & UINT64_C(0x3333333333333333)) <<  2);
+    v = ((v >>  4) & UINT64_C(0x0F0F0F0F0F0F0F0F)) | ((v & UINT64_C(0x0F0F0F0F0F0F0F0F)) <<  4);
+    v = ((v >>  8) & UINT64_C(0x00FF00FF00FF00FF)) | ((v & UINT64_C(0x00FF00FF00FF00FF)) <<  8);
+    v = ((v >> 16) & UINT64_C(0x0000FFFF0000FFFF)) | ((v & UINT64_C(0x0000FFFF0000FFFF)) << 16);
+  // clang-format on
+  return (v >> 32) | (v << 32);
+}
+
+// static void __mm_clmulepi64_si128_u64(uint64_t c[2], uint64_t a[2], uint64_t b[2], int imm8)
+static void __mm_clmulepi64_si128_u64(uint64_t *c, uint64_t *a, uint64_t *b, int imm8)
+{
+  c[0] = __clmul_u64(a[imm8 & 1], b[(imm8 >> 4) & 1]);
+
+  uint64_t ta = __bitreverse_u64(a[imm8 & 1]);
+  uint64_t tb = __bitreverse_u64(b[(imm8 >> 4) & 1]);
+  c[1] = __bitreverse_u64(__clmul_u64(ta, tb)) >> 1;
+}
+
+static void ___mm_clmulepi64_si128(uint8_t *c, uint8_t *a, uint8_t *b, int imm8)
+{
+  __mm_clmulepi64_si128_u64((uint64_t *)c, (uint64_t *)a, (uint64_t *)b, imm8);
+}
+#endif
 
 #endif
