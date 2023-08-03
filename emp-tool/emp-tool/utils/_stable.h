e17d0267edbfea5c25b62cf19f875424cfe5d293
ujnss <unlinunlin@163.com>
2023-08-03 14:21:18 +0800

    opt, use wasm-aes

---
+++ emp-tool/emp-tool/utils/_stable.h
@@ -0,0 +1,12 @@
+#ifndef STABLE_H
+#define STABLE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// Only used for key expasion.
+extern uint8_t S[256];
+// Only used for key expasion.
+extern uint8_t RC[10];
+
+#endif
