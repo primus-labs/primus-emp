b639902ef7c0e0992f6afe47d1d38ee9700b281b
xiaojiqing <xiaojiqing@padolabs.org>
2024-09-29 20:11:19 -0700

    catch pool exception

--- emp-tool/emp-tool/emp-tool.cpp
+++ emp-tool/emp-tool/emp-tool.cpp
@@ -1,6 +1,7 @@
 #include "emp-tool/execution/circuit_execution.h"
 #include "emp-tool/execution/protocol_execution.h"
 #include "emp-tool/utils/utils.h"
+#include "emp-tool/utils/ThreadPool.h"
 
 #ifndef THREADING
 emp::ProtocolExecution* emp::ProtocolExecution::prot_exec = nullptr;
@@ -11,3 +12,17 @@ __thread emp::ProtocolExecution* emp::ProtocolExecution::prot_exec = nullptr;
 __thread emp::CircuitExecution* emp::CircuitExecution::circ_exec = nullptr;
 __thread int emp::runtime_errno = 0;
 #endif
+
+void ThreadPoolSafeRun(ThreadPool* pool) {
+    try {
+        pool->runInLoop();
+    }
+    catch (std::exception& e) {
+        printf("catch in ThreadPoolSafeRun exception\n");
+        pool->stopWithReason(e.what());
+    }
+    catch (...) {
+        printf("catch in ThreadPoolSafeRun ...\n");
+        pool->stopWithReason("[OtherError]unknown exception");
+    }
+}
