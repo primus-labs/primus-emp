4cd833dd2fd82e0575e3f1980589dc3e6bbb4d28
ujnss <unlinunlin@163.com>
2024-12-20 06:05:32 +0000

    update test script

--- test.sh
+++ test.sh
@@ -14,13 +14,18 @@ emp_repos=(
 # ######################
 for repo in ${emp_repos[@]}; do
   echo "test ${repo}"
-  repo_dir=${curdir}/${repo}
+  repo_build_dir=${builddir}/${repo}
+  if [ -d "${repo_build_dir}" ]; then
+    repo_dir=${curdir}/${repo}
 
-  cd ${curdir}
-  cp -f ${repo_dir}/run ${builddir}/${repo}/
-  cd ${builddir}/${repo}/
-  # make test
-  ctest -V --rerun-failed --output-on-failure
+    cd ${curdir}
+    cp -f ${repo_dir}/run ${repo_build_dir}/
+    cd ${repo_build_dir}/
+    # make test
+    ctest -V --rerun-failed --output-on-failure
+  else
+    echo "please compile first"
+  fi
 done
 
 cd ${curdir}
